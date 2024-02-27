451e179c439bb9f39371e813fdb10bcbf646e95c
rohanNkhaire <rohanNkhaire@gmail.com>
2024-02-27 00:17:14 -0700

    Fixed NMPC formulation and added goal pose setting

--- planning/mpc_planner/include/mpc_planner/mpc_planner.hpp
+++ planning/mpc_planner/include/mpc_planner/mpc_planner.hpp
@@ -11,6 +11,10 @@
 #include <autoware_planning_msgs/msg/path.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 
+// TF2
+#include <tf2/LinearMath/Quaternion.h>
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+
 // Importing lanelet2 headers
 #include <lanelet2_extension/utility/message_conversion.hpp>
 #include <lanelet2_extension/utility/query.hpp>
@@ -29,11 +33,11 @@
 	constexpr int num_states = 4;
   constexpr int num_output = 2;
   constexpr int num_inputs = 2;
-  constexpr int pred_hor = 10;
-  constexpr int ctrl_hor = 10;
+  constexpr int pred_hor = 30;
+  constexpr int ctrl_hor = 30;
   constexpr int ineq_c = 0;
   constexpr int eq_c = 0;
-  double ts = 0.02;
+  double ts = 0.1;
 
 	mpc::NLMPC<
       num_states, num_inputs, num_output,
@@ -58,6 +62,7 @@ private:
 	// callbacks
 	void odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr&);
 	void map_callback(const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr&);
+	void global_path_callback(const autoware_planning_msgs::msg::Path::ConstSharedPtr&);
 
 	//timer
 	void timer_callback();
@@ -66,12 +71,16 @@ private:
 	nav_msgs::msg::Odometry odometry_;
 	nav_msgs::msg::Odometry prev_odometry_;
 	autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr map_ptr_;
+	autoware_planning_msgs::msg::Path path_msg_;
 	double acceleration_;
 	double prev_twist_ = 0.0;
 	bool initialized = false;
+	bool path_recieved_ = false;
 	double init_timer_;
-
-
+	double curr_velocity_;
+	double LOOK_AHEAD_TIME = 4.0;
+	int MIN_GOAL_IDX = 10;
+	mpc::cvec<4> yref;
 
 	// Lanelet
 	lanelet::LaneletMapPtr lanelet_map_ptr_;
@@ -80,7 +89,10 @@ private:
   lanelet::ConstLanelets road_lanelets_;
 
   // Functions
+	geometry_msgs::msg::Pose setGoal(autoware_planning_msgs::msg::Path&, double&, int&);
+	int getCurrentIndex(std::vector<autoware_planning_msgs::msg::PathPoint>&, nav_msgs::msg::Odometry&);
   void setMPCProblem();
+
 
 	
 };
--- planning/mpc_planner/src/mpc_planner.cpp
+++ planning/mpc_planner/src/mpc_planner.cpp
@@ -4,14 +4,21 @@ MPCPlanner::MPCPlanner() : Node("mpc_planner")
 {
 	odom_sub_ = create_subscription<nav_msgs::msg::Odometry>("/localization/kinematic_state", rclcpp::QoS{10},
 																				std::bind(&MPCPlanner::odom_callback, this, std::placeholders::_1));
+
+	global_path_sub_ = create_subscription<autoware_planning_msgs::msg::Path>("/lanelet_mission_planner/path", rclcpp::QoS{1},
+																									std::bind(&MPCPlanner::global_path_callback, this, std::placeholders::_1));
+
 	const auto durable_qos = rclcpp::QoS(1).transient_local();
   map_sub_ = create_subscription<autoware_auto_mapping_msgs::msg::HADMapBin>("/map/vector_map", durable_qos,
-																								std::bind(&MPCPLanner::map_callback, this, std::placeholders::_1));
+																								std::bind(&MPCPlanner::map_callback, this, std::placeholders::_1));
 
   trajectory_pub_ = create_publisher<autoware_planning_msgs::msg::Trajectory>("/mpc_planner/traj", rclcpp::QoS{10});
 
 	// Timer
-	auto timer_ = create_wall_timer(std::chrono::milliseconds(20), std::bind(&MPCPlanner::timer_callback, this))
+	auto timer_ = create_wall_timer(std::chrono::milliseconds(20), std::bind(&MPCPlanner::timer_callback, this));
+
+	// Init NMPC
+	setMPCProblem();
 																																												
 }
 
@@ -37,7 +44,7 @@ void MPCPlanner::odom_callback(const nav_msgs::msg::Odometry::ConstSharedPtr& ms
 	prev_odometry_ = odometry_;								
 }
 
-void MPCPlanner::map_callback(const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr msg)
+void MPCPlanner::map_callback(const autoware_auto_mapping_msgs::msg::HADMapBin::ConstSharedPtr& msg)
 {
   map_ptr_ = msg;
 
@@ -49,27 +56,65 @@ void MPCPlanner::map_callback(const autoware_auto_mapping_msgs::msg::HADMapBin::
   road_lanelets_ = lanelet::utils::query::roadLanelets(all_lanelets);
 }
 
+void MPCPlanner::global_path_callback(const autoware_planning_msgs::msg::Path::ConstSharedPtr& msg)
+{
+	path_msg_ = *msg;
+	path_recieved_ = true;
+}
+
 void MPCPlanner::timer_callback()
 {
-	// Initialize MPC
-	mpc::cvec<num_states> modelX, modeldX;
+	if(path_recieved_)
+	{
+		// Get current index on the global path
+		int curr_index = getCurrentIndex(path_msg_.points, odometry_);
 
-  modelX.resize(num_states);
-  modeldX.resize(num_states);
-  modelX(0) = odometry_.pose.pose.position.x;
-  modelX(1) = odometry_.pose.pose.position.y;
-	modelX(2) = odometry_.twist.twist.linear.x;
-	modelX(3) = odometry_.twist.twist.angular.z;
+		// Set the goal point
+		geometry_msgs::msg::Pose goal_pose = setGoal(path_msg_, odometry_.twist.twist.linear.x, curr_index);
 
-	auto r = controller.getLastResult();
+		// Get Goal Yaw
+    tf2::Quaternion goal_quat_;
+    tf2::Matrix3x3 m;
+    tf2::fromMsg(goal_pose.orientation, goal_quat_);
+    m.setRotation(goal_quat_);
+    double goal_roll, goal_pitch, goal_yaw;
+    m.getRPY(goal_roll, goal_pitch, goal_yaw);
 
-	r = controller.step(modelX, r.cmd);
+		// Set the reference for MPC
+		yref(0) = goal_pose.position.x;
+		yref(1) = goal_pose.position.y;
+		yref(3) = goal_yaw;
+		yref(4) = 5.0;
 
-	std::cout << controller.getExecutionStats();
+		// Get Vehicle Yaw
+    tf2::Quaternion veh_quat_;
+    tf2::Matrix3x3 m;
+    tf2::fromMsg(odometry_.pose.pose.orientation, veh_quat_);
+    m.setRotation(veh_quat_);
+    double veh_roll, veh_pitch, veh_yaw;
+    m.getRPY(veh_roll, veh_pitch, veh_yaw);
+
+		// Initialize MPC
+		mpc::cvec<num_states> modelX, modeldX;
+
+  	modelX.resize(num_states);
+  	modeldX.resize(num_states);
+  	modelX(0) = odometry_.pose.pose.position.x;
+  	modelX(1) = odometry_.pose.pose.position.y;
+		modelX(2) = veh_yaw;
+		modelX(3) = odometry_.twist.twist.linear.x;
+
+		auto r = controller.getLastResult();
+
+		r = controller.step(modelX, r.cmd);
+
+		std::cout << controller.getExecutionStats();
+	}
 }
 
 void MPCPlanner::setMPCProblem()
 {
+
 
 	controller.setLoggerLevel(mpc::Logger::log_level::NORMAL);
   controller.setContinuosTimeModel(ts);		
@@ -79,8 +124,8 @@ void MPCPlanner::setMPCProblem()
                        const mpc::cvec<num_states> &x,
                        const mpc::cvec<num_inputs> &u)
     {
-        dx(0) = x(4)*cos(x(3));
-        dx(1) = x(4)*sin(x(3));
+        dx(0) = x(3)*cos(x(2));
+        dx(1) = x(3)*sin(x(2));
 				dx(2) = u(0);
 				dx(3) = u(1);
     };
@@ -97,7 +142,65 @@ void MPCPlanner::setMPCProblem()
                                        const mpc::mat<pred_hor + 1, num_output> &,
                                        const mpc::mat<pred_hor + 1, num_inputs> &u,
                                        double)
-                                   { return x.array().square().sum() + u.array().square().sum(); });
+                                   { 
+																			double cost = 0;
+        							for (int i = 0; i < (pred_hor + 1); ++i)
+        							{
+        							    cost += (x.row(i).segment(0, 2).transpose() - yref.segment(0, 2)).squaredNorm();
+        							    cost += u.row(i).squaredNorm();
+													cost += std::norm(x(i, 3) - yref(3));
+													cost += std::norm(x(i, 2) - yref(2));
+													cost += i > 0 ? std::norm(u(i) - u(i-1)) : 0.0;
+        							}
+
+        							return cost;
+									
+									
+									return x.array().square().sum() + u.array().square().sum(); });
 
 																 
+}
+
+geometry_msgs::msg::Pose MPCPlanner::setGoal(autoware_planning_msgs::msg::Path& path, double& curr_vel, int& curr_idx)
+{
+	// Set goal such that we plan for 4s minimum
+	// We take goal from the global planner
+	// scale goal with vehicle speed
+	int max_length = path.points.size() - 1;
+	int min_goal_idx = std::min(curr_idx + MIN_GOAL_IDX, max_length);
+	int goal_idx = std::min(curr_idx + int(std::round(curr_vel*LOOK_AHEAD_TIME)), min_goal_idx);
+	geometry_msgs::msg::Pose goal_pose = path.points.at(goal_idx).pose;
+
+	return goal_pose;
+}
+
+int getCurrentIndex(std::vector<autoware_planning_msgs::msg::PathPoint>& path_point, nav_msgs::msg::Odometry& veh_odom)
+{
+	int curr_index = findNearestIndex(path_point, veh_odom.pose.pose.position);
+	return curr_index;
+}
+
+size_t findNearestIndex(std::vector<autoware_planning_msgs::msg::PathPoint>& points, const geometry_msgs::msg::Point& point)
+{
+
+  double min_dist = std::numeric_limits<double>::max();
+  size_t min_idx = 0;
+
+  for (size_t i = 0; i < points.size(); ++i) {
+    const auto dist = calcSquaredDistance2d(points.at(i).pose.position, point);
+    if (dist < min_dist) {
+      min_dist = dist;
+      min_idx = i;
+    }
+  }
+  return min_idx;
+}
+
+double calcSquaredDistance2d(const geometry_msgs::msg::Point& point1, const geometry_msgs::msg::Point& point2)
+{
+  const auto p1 = point1;
+  const auto p2 = point2;
+  const auto dx = p1.x - p2.x;
+  const auto dy = p1.y - p2.y;
+  return dx * dx + dy * dy;
 }
